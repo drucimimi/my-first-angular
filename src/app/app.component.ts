5b1ec06e146d7984ab7733435aeb6858d0e318d4
DEROCHE <drucilla.deroche@accenture.com>
2025-03-25 17:25:03 +0100

    Ma première app Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import {FormBuilder} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatCheckboxModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Todo';
+  title = 'Test App';
+  private readonly _formBuilder = inject(FormBuilder);
+
+  readonly toppings = this._formBuilder.group({
+    useAngularComponents: false,
+    createOwnComponents: false,
+    createOwnRoutes: false,
+  });
 }
